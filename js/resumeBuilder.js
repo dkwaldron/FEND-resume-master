b47afc08404bf46f10fc2de19906ea67b8bd1b75
dkwaldron <dkwaldron@gmail.com>
2016-11-21 15:53:29 -0500

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,27 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("David Waldron");
+
+// var awesomeThoughts = "I am David, and I am awesome!";
+//
+// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+// $("#main").append(funThoughts);
+
+
+// Resume header
+var fullName = "David Waldron";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace ("%data%", fullName);
+var formattedRole = HTMLheaderRole.replace ("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+// Skills
+var skills = ["awesomeness", "programming", "teaching", "JS"]
+
+$("#main").append(skills);
